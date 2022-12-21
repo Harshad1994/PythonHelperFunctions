7f4ee3ef281399e23c4f4482717f1639b9a8ac46
Harshad1994 <harshadkunjir@gmail.com>
2022-12-21 17:17:28 +0530

    Update ploting_graphs_and_visualisation.py

--- ploting_graphs_and_visualisation.py
+++ ploting_graphs_and_visualisation.py
@@ -20,3 +20,35 @@ def plot_continous_variable_for_two_categories(df)
     plt.title('Age distribution grouped by attrition',fontsize= 14)
     plt.ylabel('Status of employee')
     plt.tight_layout()
+    
+    
+   
+def plot_continuous_against2_variables(df):
+    plt.figure(figsize=(20, 5))
+    palette = "Set3"
+
+    plt.subplot(1, 3, 1)
+    sns.boxplot(x = 'Sex', y = 'Age', data = age_1_class,
+         palette = palette, fliersize = 0)
+    sns.stripplot(x = 'Sex', y = 'Age', data = age_1_class,
+         linewidth = 0.6, palette = palette)
+    plt.title('1st class Age distribution by Sex',fontsize= 14)
+    plt.ylim(-5, 80)
+
+    plt.subplot(1, 3, 2)
+    sns.boxplot(x = 'Sex', y = 'Age', data = age_2_class,
+         palette = palette, fliersize = 0)
+    sns.stripplot(x = 'Sex', y = 'Age', data = age_2_class,
+         linewidth = 0.6, palette = palette)
+    plt.title('2nd class Age distribution by Sex',fontsize= 14)
+    plt.ylim(-5, 80)
+
+    plt.subplot(1, 3, 3)
+    sns.boxplot(x = 'Sex', y = 'Age',  data = age_3_class,
+         order = ['female', 'male'], palette = palette, fliersize = 0)
+    sns.stripplot(x = 'Sex', y = 'Age', data = age_3_class,
+         order = ['female', 'male'], linewidth = 0.6, palette = palette)
+    plt.title('3rd class Age distribution by Sex',fontsize= 14)
+    plt.ylim(-5, 80)
+
+    plt.show()
